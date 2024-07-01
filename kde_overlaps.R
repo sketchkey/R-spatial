7e62ad87b211624478ff67d51909c28dbd035525
Stephen Lang <65951795+sketchkey@users.noreply.github.com>
2024-07-01 11:54:03 +0100

    Update kde_overlaps.R

--- kde_overlaps.R
+++ kde_overlaps.R
@@ -9,7 +9,7 @@ if(!require(pacman)) install.packages("pacman")
 pacman::p_load(here, tidyverse, sf, eks, lwgeom, terra, tidyterra, rnaturalearth, rnaturalearthdata, rnaturalearthhires) 
 
 # Calculate some random spatial GPS data (lat-lon) for two groups
-set.seed(123) #for consistent results
+set.seed(12345) #for consistent results
 n <- 100 #number of samples per group
 spatial_data <- tibble(
   id = rep(1:2, each = n),
@@ -49,13 +49,16 @@ spatial_overlap <- spatial_skde %>% filter(contlabel == "95") %>%
 # Calculate proportional overlap (total area divided by intersecting area)
 prop_overlap <- (sf::st_area(spatial_intersection) / sf::st_area(spatial_overlap)) * 100
 
-#Do final plot as above but including the intersection with a different colour and add the prop_overlap as text in figure
-ggplot(data = spatial_skde, aes(geometry = geometry, group = id)) + 
-  geom_sf(aes(fill = contlabel, colour = contlabel)) +
-  geom_sf(data = spatial_intersection, fill = "black", colour = "black", alpha = 0.5) +
-  geom_text(x = 19.75, y = -34.0, aes(label = paste("KDE overlap", round(prop_overlap, 2), "%")), colour = "black", size = 3.5) +
-  scale_fill_manual(values = c("50" = "#E41A1CB3", "95" = NA), na.value = NA) +
-  scale_colour_manual(values = c("50" = NA, "95" = "#E41A1CB3"), na.value = NA) +
+# Make final skde object with tweaked ID column for the two groups
+spatial_skde_final <- spatial_skde %>%
+  mutate(id_contlabel = paste(id, contlabel, sep = "_"))
+
+ggplot(data = spatial_skde_final, aes(geometry = geometry, group = id)) + 
+  geom_sf(aes(fill = id_contlabel, colour = id_contlabel)) +
+  geom_sf(data = spatial_intersection, fill = "grey20", colour = "black", alpha = 0.5) +
+  geom_text(x = 19.75, y = -34.0, aes(label = paste("KDE overlap:", round(prop_overlap, 2), "%")), colour = "grey30", size = 3.5) +
+  scale_fill_manual(values = c("1_50" = "#E41A1CB3", "1_95" = NA, "2_50" = "#377EB8B3", "2_95" = NA), na.value = NA) +
+  scale_colour_manual(values = c("1_50" = NA, "1_95" = "#E41A1CB3", "2_50" = NA, "2_95" = "#377EB8B3"), na.value = NA) +
   labs(x = "Longitude", y = "Latitude", fill = "KDE", colour = "KDE") +
   theme_bw(base_size = 9) +
-  theme(strip.background =element_rect(fill="grey95"))
+  theme(strip.background = element_rect(fill="grey95"))
